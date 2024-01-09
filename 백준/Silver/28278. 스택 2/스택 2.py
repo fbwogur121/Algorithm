f1b83a6cf7a4df78e8b3ba73ac8030f4c515f44e
Rjhkki222 <65850233+fbwogur121@users.noreply.github.com>
2024-01-09 10:40:57 +0900

    [Silver IV] Title: 스택 2, Time: 908 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for _ in range(N):
+    a = input().rstrip()
+    if len(a)>2:
+        stack.append(int(a[2:]))
+    elif a=='2':
+        if(len(stack)==0): print(-1)
+        else: print(stack.pop())
+        
+    elif a=='3':
+        print(len(stack))
+    elif a=='4':
+        if(len(stack)==0): print(1)
+        else: print(0)
+    elif a=='5':
+        if(len(stack)==0): print(-1)
+        else: print(stack[-1])

e3afa448ff5c7a4bbff66b6e184c821d93979289
Rjhkki222 <65850233+fbwogur121@users.noreply.github.com>
2024-11-13 12:56:40 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 696 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N, P = map(int, input().split())
+cnt = 0
+lst = [[] for _ in range(7)]
+
+for i in range(N):
+    n, p = map(int, input().split())
+    if not lst[n-1]:
+        lst[n-1].append(p)
+        cnt += 1
+    else:
+        while lst[n-1] and p < lst[n-1][-1]:
+            lst[n-1].pop()
+            cnt += 1
+        if not lst[n-1] or p > lst[n-1][-1]:
+            lst[n-1].append(p)
+            cnt += 1
+        else:
+            pass
+print(cnt)

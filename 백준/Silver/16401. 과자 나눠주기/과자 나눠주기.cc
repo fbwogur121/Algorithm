eb2cd4333ec8e91b2821b5136689b412ea92b257
Rjhkki222 <65850233+fbwogur121@users.noreply.github.com>
2025-02-02 22:27:14 +0900

    [Silver II] Title: 과자 나눠주기, Time: 640 ms, Memory: 8180 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int m, n, input;
+    cin >> m >> n;
+
+    vector<int> snacks;
+
+    for(int i=0; i<n; i++){
+        cin >> input;
+        snacks.push_back(input);
+    }
+
+    sort(snacks.begin(), snacks.end());
+
+    int low = 1;
+    int high = snacks[n-1];
+
+    int result = 0;
+
+    while(low <= high){
+        int mid = (low + high)/2;
+
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            cnt += snacks[i]/mid;    
+        }
+
+        if(cnt < m){
+            high = mid - 1;
+            
+        }
+        else{
+            result = mid;
+            low = mid + 1;
+        }
+    }
+    cout << result;
+
+    return 0;
+}

9c4c18456763f8782824b5f2f3a9e8488b11e922
Rjhkki222 <65850233+fbwogur121@users.noreply.github.com>
2025-03-30 23:31:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,18 +1,12 @@
-def A(n,k):
-    up=1
-    down=1 
-    for i in range(k):
-        up *= (n-i)
-        down *= (k-i)
-    
-    return((up//down)%10007)
-
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-sum = 0
+dp =[0] *1001
+dp[1] = 1
+dp[2] = 2
 
-for i in range(int(n/2) + 1):
-    sum += A(n-i, i)
-print(sum%10007)
+for i in range(3,n+1): 
+    dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    
+print(dp[n])
